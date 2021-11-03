e374f20010636b55041ba853db23b7e54f11a7a7
ldalzate <ldalzatef@unal.edu.co>
2021-11-02 22:06:03 -0500

    Creamos la clase control

---
+++ televisores/control.py
@@ -0,0 +1,22 @@
+Class Control:
+
+    def __init__(self):
+        self._tv = None
+    
+    def turnOn(self):
+        self._tv.turnOn()
+    
+    def turnOff(self):
+        self._tv.turnOff()
+
+    def canalUp(self):
+        self._tv.canalUp()
+
+    def canalDown(self):
+        self._tv.canalDown()
+
+    def VolumenlUp(self):
+        self._tv.volumenUp()
+
+    def VolumenlDown(self):
+        self._tv.volumenDown()
